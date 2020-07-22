aa169487421e99ce31b5964ff86b3e3e9481a9ba
student <s.katrina@y7mail.com>
2020-07-22 16:19:41 +1000

    subtask 1 and 2 working

--- server.js
+++ server.js
@@ -1,10 +1,19 @@
 var express = require('express')
 var app = express()
 
-//respond with "hello world" when a GET request is made to the homepage
-app.get('/', function (req,res){
-    res.send('hello world')
-})
+app.use(express.static(__dirname + '/public'));
+//listen to a particular port (default 3000)
+var port = 3000;
+app.listen(port)
 
-//list(en?) to a particular port
-app.listen(3000)
+var addInts = function(num1,num2){
+    var total = parseInt(num1) + parseInt(num2);
+    return total;
+}
+
+app.get('/add', function(req,res){
+    var num1 = req.query.num1;
+    var num2 = req.query.num2;
+    var total = addInts(num1,num2);
+    res.send('The total of '+num1+' and '+num2+' is '+total);
+})
