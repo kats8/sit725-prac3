b57c08cf17d5c985ab32726eef01f5102715fc77
Katrina Steen <s.katrina@y7mail.com>
2020-07-27 16:42:15 +1000

    array retreival by name, id, deposit or all working

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express')
 var app = express()
 let accounts = [{ id: 1, name: 'alex', deposit: 5 },
 { id: 2, name: 'sarah', deposit: 5 },
-{ id: 3, name: 'jim', deposit: 15 }]
+{ id: 3, name: 'jim', deposit: 15 }];
 
 app.use(express.static(__dirname + '/public'));
 //listen to a particular port (default 3000)
@@ -21,16 +21,88 @@ app.get('/add', function (req, res) {
     res.send('The total of ' + num1 + ' and ' + num2 + ' is ' + total);
 })
 
-app.get('/find_account', function (req, res) {
+//finds account/s matching provided name
+app.get('/find_account_name', function (req, res) {
     let name = req.query.name;
+    let displayString = "";
+    let found = false;
+    console.log(accounts.length);
     for (i = 0; i < accounts.length; i++) {
-        if (name == accounts[i].name) {
+        if (name.toUpperCase() == (accounts[i].name).toUpperCase()) {
             let matchID = accounts[i].id;
             let matchName = accounts[i].name;
             let matchDep = accounts[i].deposit;
-            res.send("Matching account found\n ID: ${matchID}\nName: ${matchName}\n Deposit: ${matchDep}")
-        } else {
-            res.send("No matching account found.");
+            displayString = displayString + "<p>Matching account found:</p><p>Account ID: " + matchID + "<br>Name: " + matchName + "<br>Deposit: " + matchDep + "</p>";
+            found = true;
         }
     }
+    if (found == true) {
+        res.send(displayString);
+    }
+    else {
+        res.send("No matching account found. ");
+    }
+})
+
+//returns accounts matching provided ID
+app.get('/find_account_ID', function (req, res) {
+    let id = req.query.id;
+    let found = false;
+    let displayString = "";
+    for (i = 0; i < accounts.length; i++) {
+        if (id == accounts[i].id) {
+            let matchID = accounts[i].id;
+            let matchName = accounts[i].name;
+            let matchDep = accounts[i].deposit;
+            displayString = displayString + "<p>Matching account found:</p><p>Account ID: " + matchID + "<br>Name: " + matchName + "<br>Deposit: " + matchDep + "</p>";
+            found = true;
+        }
+    }
+    if (found == true) {
+        res.send(displayString);
+    }
+    else {
+        res.send("No matching account found. ");
+    }
+})
+
+//finds accounts matching provided deposit amount
+app.get('/find_account_deposit', function (req, res) {
+    let deposit = req.query.deposit;
+    let displayString = "";
+    let found = false;
+    for (i = 0; i < accounts.length; i++) {
+        if (parseInt(deposit) == parseInt(accounts[i].deposit)) {
+            let matchID = accounts[i].id;
+            let matchName = accounts[i].name;
+            let matchDep = accounts[i].deposit;
+            displayString = displayString + "<p>Matching account found:</p><p>Account ID: " + matchID + "<br>Name: " + matchName + "<br>Deposit: " + matchDep + "</p>";
+            found = true;
+        }
+    }
+    if (found == true) {
+        res.send(displayString);
+    }
+    else {
+        res.send("No matching account found. ");
+    }
+})
+
+//retrieves all accounts
+app.get('/find_accounts_all', function (req, res) {
+    let displayString = "";
+    if (accounts.length > 0) {
+        for (i = 0; i < accounts.length; i++) {
+            let matchID = accounts[i].id;
+            let matchName = accounts[i].name;
+            let matchDep = accounts[i].deposit;
+            displayString = displayString + "<p>Account ID: " + matchID + "<br>Name: " + matchName + "<br>Deposit: " + matchDep + "</p>";
+        }
+        res.send(displayString);
+
+    }
+    else {
+        res.send("Account list empty.");
+    }
+
 })
